95c34670bc385546f950aa4a454a2f0801dd3be1
rui9294 <ruine.gaming@gmail.com>
2025-06-16 23:14:09 +0900

    SecondScene作成

--- Assets/Scripts/PlayerDotMove.cs
+++ Assets/Scripts/PlayerDotMove.cs
@@ -1,12 +1,24 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDotMove : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
+    private bool verticalOnly = false;
+
+    void Start()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName == "SecondScene")
+        {
+            verticalOnly = true;
+        }
+    }
+
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+        float horizontal = verticalOnly ? 0f : Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         Vector3 move = new Vector3(horizontal, vertical, 0f) * moveSpeed * Time.deltaTime;
